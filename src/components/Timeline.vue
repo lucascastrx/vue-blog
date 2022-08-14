d60f447d68d38e3991f32f707cd7db9957908824
Lucas <luukascastro@gmail.com>
2022-08-13 21:13:25 -0300

    feat: pinia state management added

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,49 +1,28 @@
 <script setup lang="ts">
 import { DateTime } from 'luxon';
 import { ref, computed } from 'vue';
-import {Post, today, thisWeek, thisMonth} from '../posts'
+import {TimelinePost, today, thisWeek, thisMonth} from '../posts'
+import TimelineItem from './TimelineItem.vue'
+import {usePosts} from '../stores/posts' 
+import { periods, Period} from '../constants'
 
-const periods = ['Today', 'This week', 'This month'] as const
+const postsStore = usePosts()
+await postsStore.fetchPosts()
 
-type Period = typeof periods[number]
 
-
-const selectedPeriod = ref<Period>('Today')
-
-function selectPeriod(period: Period) {
-  selectedPeriod.value = period
-}
-
-const posts = computed(() =>{ 
- return [today, thisWeek, thisMonth].map(post =>{
-  return {
-    ...post,
-    created: DateTime.fromISO(post.created)
-  }
-}).filter(post =>{
-  if(selectedPeriod.value === 'Today'){
-    return post.created >= DateTime.now().minus({day:1})
-  }
-
-  if(selectedPeriod.value === 'This week'){
-    return post.created >= DateTime.now().minus({week:1})
-  }
-
-  return post
-})
-})
  
 </script>
 
 <template>
+  {{}}
   <nav class="is-primary panel">
-    {{selectedPeriod}}
+    {{postsStore.selectedPeriod}}
     <span class="panel-tabs">
-      <a v-for="period of periods" :key="period" @click="selectPeriod(period)" :class="{ 'is-active': period === selectedPeriod}">
+      <a v-for="period of periods" :key="period" @click="postsStore.setSelectedPeriod(period)" :class="{ 'is-active': period === postsStore.selectedPeriod}">
         {{period}}
       </a>
-      
     </span>
+      <TimelineItem v-for="post of postsStore.filteredPosts" :key="post.id" :post="post"/>
   </nav>
 </template>
 
